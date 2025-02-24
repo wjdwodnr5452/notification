32060229fc002dd383c0dd99f3504c6d0cff85a4
wjdwodnr5452 <wjdwodnr5452@naver.com>
2025-02-24 22:39:10 +0900

    kafka consumer 메시지 수발신 설정

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -4,5 +4,6 @@ dependencies {
     implementation(project(mapOf("path" to ":core")))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka")
+    implementation("org.springframework.cloud:spring-cloud-starter-function-web") // 추가
 }
 
--- consumer/src/main/java/com/kafka/consumer/NotificationConsumerApplication.java
+++ consumer/src/main/java/com/kafka/consumer/NotificationConsumerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotificationConsumerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationConsumerApplication.class, args);
 	}
-
 }
