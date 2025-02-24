c5d84e07517828820c0543479c825e143f921a76
wjdwodnr5452 <wjdwodnr5452@naver.com>
2025-02-24 20:38:29 +0900

    kafka 설정

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -1,9 +1,8 @@
 
-
 dependencies {
     // core 모듈 추가
     implementation(project(mapOf("path" to ":core")))
     implementation("org.springframework.boot:spring-boot-starter-web")
-
+    implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka")
 }
 
