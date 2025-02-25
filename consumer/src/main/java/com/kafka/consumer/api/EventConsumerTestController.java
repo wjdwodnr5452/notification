7147c6e0eedb08fa9c268127aa5e36b103941126
wjdwodnr5452 <wjdwodnr5452@naver.com>
2025-02-25 23:22:49 +0900

    spring doc 으로 api 테스트

--- consumer/src/main/java/com/kafka/consumer/api/EventConsumerTestController.java
+++ consumer/src/main/java/com/kafka/consumer/api/EventConsumerTestController.java
@@ -3,18 +3,21 @@ package com.kafka.consumer.api;
 import com.kafka.consumer.event.CommentEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.function.Consumer;
 
 @RestController
-public class EventConsumerTestController {
+public class EventConsumerTestController implements EventConsumerTestControllerSpec {
 
     @Autowired
     private Consumer<CommentEvent> comment;
 
+
+    @Override
     @PostMapping("/test/comment")
-    public void comment(CommentEvent event) {
+    public void comment(@RequestBody CommentEvent event) {
         comment.accept(event);
     }
 
